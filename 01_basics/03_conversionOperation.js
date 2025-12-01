42aeeae182e5f4bf12c7839d711086de99391428
Farhan Ahmed Khan <farhannnkhan770@gmail.com>
2025-12-01 13:07:19 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,3 +4,11 @@ console.log(typeof score);
 let valueInNumber = Number(score);      // converting string to number
 console.log(typeof valueInNumber);      // this is giving that the variable has number
 console.log(valueInNumber);             // but when we print this number it gives NAN
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+let Numbers = 33;
+let stringNumber = String(Numbers)
+console.log(typeof stringNumber);
