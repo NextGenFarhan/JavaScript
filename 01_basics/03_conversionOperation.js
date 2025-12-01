990563e935ec0cc66f0d8d29cf24a97646cece18
Farhan Ahmed Khan <farhannnkhan770@gmail.com>
2025-12-01 13:20:05 +0000

    operations and conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,14 +1,29 @@
-// let score = "33";
-let score = "33abc";
-console.log(typeof score);
-let valueInNumber = Number(score);      // converting string to number
-console.log(typeof valueInNumber);      // this is giving that the variable has number
-console.log(valueInNumber);             // but when we print this number it gives NAN
+// // let score = "33";
+// let score = "33abc";
+// console.log(typeof score);
+// let valueInNumber = Number(score);      // converting string to number
+// console.log(typeof valueInNumber);      // this is giving that the variable has number
+// console.log(valueInNumber);             // but when we print this number it gives NAN
 
-let isLoggedIn = 1;
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn)
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
 
-let Numbers = 33;
-let stringNumber = String(Numbers)
-console.log(typeof stringNumber);
+// let Numbers = 33;
+// let stringNumber = String(Numbers)
+// console.log(typeof stringNumber);
+
+
+// ******************************************* CONVERSION ************************************************
+let value = 3;
+let negValue = -value;
+console.log(negValue)
+
+let str1 = "Hello"
+let str2 = "Farhan"
+console.log(str1+str2)
+
+// NOW string aur numbers pe operation
+console.log(1+"2")              // 1 add kiya fir 2 ko string consider krke baad me concat kr diya
+console.log("2"+1+1)            // 2 ko string samjha fir uske baad ke numbers ko bhi string consider kiya 
+console.log(1+1+"2")            // pehle 2 toh numbers consider karega fir last wale 2 ko string lelega
