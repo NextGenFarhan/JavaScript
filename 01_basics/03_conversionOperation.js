733c96032630f94aa10b63018671e426f301ef4f
Farhan Ahmed Khan <farhannnkhan770@gmail.com>
2025-12-01 12:34:01 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,6 @@
+// let score = "33";
+let score = "33abc";
+console.log(typeof score);
+let valueInNumber = Number(score);      // converting string to number
+console.log(typeof valueInNumber);      // this is giving that the variable has number
+console.log(valueInNumber);             // but when we print this number it gives NAN
