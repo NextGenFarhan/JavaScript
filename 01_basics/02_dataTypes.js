b76b952e8107b754b3aabf0b38834259bf073d0d
Farhan Ahmed Khan <farhannnkhan770@gmail.com>
2025-12-01 12:25:40 +0000

    container cofig done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,43 @@
+// 1] string 
+let color = "Yellow";
+let lastName = 'Johnson';
+let greeting = `Hello, ${color}`; // Backticks allow string interpolation
+
+const goodQuotes1 = 'She said "I think so!" ';
+const goodQuotes2 = `She said "I'm not going in there!"`;
+console.log(goodQuotes1 + goodQuotes2);
+const bigmouth = 'I\'ve got no right to take my place…';
+console.log(bigmouth);
+console.log(typeof goodQuotes1, typeof goodQuotes2, typeof bigmouth)     // to check the type of datatype entered
+
+
+// 2] NUMBER 
+let length = 16;
+let weight = 7.5;
+
+// 3] BIG INT 
+let a = 1234567890123456789012345n;
+let b = BigInt(1234567890123456789012345n);
+
+// 4] Boolean
+let c = true;
+let d = false;
+
+// 5] NULL
+let g = null;
+
+// 6] UNDEFINED
+let e;
+let f;
+
+// 7. SYMBOL 
+const i = Symbol();
+const j = Symbol();
+
+// 8] OBJECT : non primitive data type
+const person = {firstName:"John", lastName:"Doe"};
+// Array object
+const cars = ["Saab", "Volvo", "BMW"];
+// Date object
+const date = new Date("2022-03-25");
+
