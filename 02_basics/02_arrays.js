5e936656a78b6e796e7629b14a05c93220d1d31f
Farhan Ahmed Khan <farhannnkhan770@gmail.com>
2025-12-02 12:28:22 +0000

    file

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,9 @@
+let a1 = [1, 5, 3, 8]
+let a2 = [2, 4, 6]
+let a3 = [7, 8, 9]
+
+console.log()
+console.log(a1.concat(a2, a3))             // add
+console.log(a1.sort())                     // sorts the array is ascending order
+console.log(a1.splice(1, 2))               // jo delete hue woh print karega
+console.log(a1.splice(1, 2, 333, 444))     // 333 and 444 add ho jaayenge
