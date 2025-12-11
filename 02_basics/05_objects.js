67a281bdea4a49cf27efbccf6630f607647ed524
Farhan Ahmed Khan <farhannnkhan770@gmail.com>
2025-12-11 14:04:21 +0000

    json object  basic

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,18 @@
+// destructuring of object
+const course = {
+    courseName: "abdsa",
+    price: "999999",
+    courseIntructor: "Farhan"
+}
+// course.courseIntructor
+const {courseIntructor} = course
+console.log(courseIntructor)
+
+
+// API's
+// from api value comes in json form
+// {
+//     name1 : "dmnansa",
+//     coureName: "dsmahe", 
+//     price: "100000000000009"
+// }
