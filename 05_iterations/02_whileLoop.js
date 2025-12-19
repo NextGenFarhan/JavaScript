3d6971eafca34828b9c3adbf91d9146205b745fe
Farhan Ahmed Khan <farhannnkhan770@gmail.com>
2025-12-19 18:18:55 +0000

    while loop

---
+++ 05_iterations/02_whileLoop.js
@@ -0,0 +1,25 @@
+while (2==2) {
+    // console.log("is true")
+    break
+}
+
+let i=1;
+while(i<=10) {
+    // console.log(`Value of index is : ${i}`);
+    i++;
+}
+
+
+let myArr = ["flash", "batman", "superman"]
+let arr = 0;
+while(arr < myArr.length) {
+    // console.log(`Value is ${myArr[arr]}`)
+    arr++;
+}
+
+
+let score = 6;
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while(score<=10)
